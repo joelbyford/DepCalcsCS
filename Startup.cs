32e64cabfd295d558f88f8467b0faec14a88930d
Joel Byford <jobyford@microsoft.com>
2021-04-21 11:47:13 -0700

    Adding BasicAuth as an option to the service.

--- Startup.cs
+++ Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using joelbyford;
+using System.IO;
+using System.Text.Json;
 
 namespace DepCalcsCS
 {
@@ -42,6 +45,22 @@ namespace DepCalcsCS
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "DepCalcsCS v1"));
+            }
+            //Insert the Basic Authentication Middleware handler *ONLY IF* it was enabled in appsettings.json
+            // see documentation at https://github.com/joelbyford/basicauth/ for more details
+            bool basicAuthEnabled = this.Configuration.GetValue<bool>("AppSettings:BasicAuth:Enabled");
+            
+            if (basicAuthEnabled)
+            {   
+                //Uses values from "BasicAuth" under "AppSettings" in the appsettings.json
+                String basicAuthRealm = this.Configuration.GetValue<String>("AppSettings:BasicAuth:Realm");
+                String basicAuthUserJson = this.Configuration.GetValue<String>("AppSettings:BasicAuth:UsersJson");
+
+                // Using the BasicAuth NuGet package from https://github.com/joelbyford/BasicAuth
+                Dictionary<string, string> basicAuthUsers = new Dictionary<string, string>();
+                var packageJson = File.ReadAllText(basicAuthUserJson);
+                basicAuthUsers = JsonSerializer.Deserialize<Dictionary<string, string>>(packageJson);
+                app.UseMiddleware<joelbyford.BasicAuth>(basicAuthRealm, basicAuthUsers);
             }
 
             app.UseHttpsRedirection();
